d25342530322bcd06eed0e410ba98c5ed346d525
Harvey <harvey@HarveydeMBP.lan>
2020-03-20 10:54:50 +0800

    通知优化

--- Source/Tools/BKNotify.swift
+++ Source/Tools/BKNotify.swift
@@ -22,30 +22,37 @@ public class BKNotify {
     
     public func remove(observer: Any,  name: BKNotify.Name? = nil, object: Any? = nil) {
         
-        if name == nil, object == nil {
-            
-            notification.removeObserver(observer)
-            return
-        }
-        
-        guard let _ = name else {
-            
-            notification.removeObserver(observer, name: nil, object: object)
-            return
-        }
-        notification.removeObserver(observer, name: Notification.Name(name!.rawValue), object: object)
+        remove(observer: observer, name: Notification.Name(name!.rawValue), object: object)
     }
     
-    public func post(name: BKNotify.Name, object: Any? = nil) {
-        notification.post(name: Notification.Name(name.rawValue), object: object)
+    public func remove(observer: Any,  name: Notification.Name? = nil, object: Any? = nil) {
+        
+        notification.removeObserver(observer, name: name, object: object)
+    }
+    
+    public func post(name: BKNotify.Name, object: Any? = nil, userInfo: [AnyHashable : Any]? = nil) {
+        post(name: Notification.Name(name.rawValue), object: object, userInfo: userInfo)
+    }
+    
+    public func post(name: NSNotification.Name, object: Any? = nil, userInfo: [AnyHashable : Any]? = nil) {
+        notification.post(name: name, object: object, userInfo: userInfo)
     }
     
     public func addObserver(name: BKNotify.Name, object: Any? = nil, queue: OperationQueue? = nil, usingblock: @escaping (Notification) -> Void) {
         
-        _ = notification.addObserver(forName: Notification.Name(name.rawValue), object: object, queue: queue, using: usingblock)
+        _ = addObserver(name: Notification.Name(name.rawValue), object: object, queue: queue, usingblock: usingblock)
+    }
+    
+    public func addObserver(name: NSNotification.Name, object: Any? = nil, queue: OperationQueue? = nil, usingblock: @escaping (Notification) -> Void) {
+        
+        _ = notification.addObserver(forName: name, object: object, queue: queue, using: usingblock)
     }
     
     public func addObserver(_ observer: Any, selector: Selector, name: BKNotify.Name, object: Any? = nil) {
-        notification.addObserver(observer, selector: selector, name: Notification.Name(name.rawValue), object: object)
+        addObserver(observer, selector: selector, name: Notification.Name(name.rawValue), object: object)
+    }
+    
+    public func addObserver(_ observer: Any, selector: Selector, name: NSNotification.Name, object: Any? = nil) {
+        notification.addObserver(observer, selector: selector, name: name, object: object)
     }
 }
