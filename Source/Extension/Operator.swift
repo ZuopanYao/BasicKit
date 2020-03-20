980a7f7c5056607254f605cc43b52400b6163283
Harvey <harvey@HarveydeMBP.lan>
2020-03-20 11:07:26 +0800

    操作符-正则表达式

---
+++ Source/Extension/Operator.swift
@@ -0,0 +1,25 @@
+//
+//  Operator.swift
+//  BasicKit
+//
+//  Created by Harvey on 2020/3/20.
+//  Copyright © 2020 姚作潘/Harvey. All rights reserved.
+//
+
+import Foundation
+
+precedencegroup RegularPrecedence {
+    
+    lowerThan: AdditionPrecedence       // 优先级, 比加法运算低
+    associativity: none                 // 结合方向:left, right or none
+    assignment: false                   // true=赋值运算符,false=非赋值运算符
+}
+
+infix operator ~~: RegularPrecedence
+
+/// 正则表达式匹配
+func ~~(origin: String, regular: String) -> Bool {
+    
+    let predicate = NSPredicate(format: "SELF MATCHES %@", regular)
+    return predicate.evaluate(with: origin)
+}
